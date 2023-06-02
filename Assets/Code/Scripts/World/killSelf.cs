d5c8f5ff673f67de1a3c67abf64de6e82c7e420f
Jlytsman <jojojoellijtsman@gmail.com>
2023-06-02 10:19:01 +0200

    Quick fix
    Quickly fixed errything

---
+++ Assets/Code/Scripts/World/killSelf.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killSelf : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+          gameObject.active = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
