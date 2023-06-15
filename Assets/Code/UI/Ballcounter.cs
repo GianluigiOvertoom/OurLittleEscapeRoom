40d030f9e92224d777638db46766f937b3b3064c
SvenLoeve <svenloeve@gmail.com>
2023-06-15 11:10:40 +0200

    Balltickler3000

---
+++ Assets/Code/UI/Ballcounter.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Ballcounter : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text _title;
+
+    public PointScoring Hoop;
+    
+    public int BallAmount;
+    public int BallCheck;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _title.text = (BallAmount.ToString());
+    }
+
+    private void Update()
+    {
+        BallAmount = Hoop.Score;
+
+        if (BallAmount != BallCheck)
+        {
+            _title.text = ("" + BallAmount);
+            BallCheck = BallAmount;
+        }
+    }
+}
